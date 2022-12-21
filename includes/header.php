9630244d51d3749a491dcab0dc79a79ba831e42f
David McPhee <david.mcphee@calian.com>
2022-12-21 15:32:54 -0600

    experiment with php includes

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+ 
+<title>David McPhee's Personal Homepage</title>
+<link rel="shortcut icon" type="image/png" href="includes/favicon.png">
+<link href="includes/main.css" rel="stylesheet" type="text/css" media="screen" />
+ 
+</head>
+ 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-C8K8BK48K1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+ 
+  gtag('config', 'G-C8K8BK48K1');
+</script>
+ 
+<body style="background-color:#121212;">
+ 
+<div id="content">
+ 
+<div id="nav">
+<ul>
+<li><a href="/projects">Back To Projects</a></li>
+</ul>
+</div>
+ 
+<!-- End Header $include_path/header.html -->
